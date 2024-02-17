88271a58e4eb4c51a41250e823e2ca3864ebb411
arpittiwari24 <atofficial2410@gmail.com>
2024-02-17 21:27:06 +0530

    wertyq

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,14 +10,14 @@ export default function Navbar () {
     const premium = usePremiumContext()
     
     return (
-        <div className="navbar ">
+        <div className="navbar bg-gradient-to-r from-blue-600 to-violet-400 text-white">
         <div className="navbar-start">
           <div className="dropdown text-2xl">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
             {session && session?.user ? (
-              <></>
+              <><SigninButton /></>
             ): (
               <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
               <li><Link href="/pricing">Pricing</Link></li>
@@ -28,8 +28,8 @@ export default function Navbar () {
             )}
           </div>
           <Link href="/" className="btn btn-ghost max-sm:text-xl text-3xl">TidyL</Link>
-          {!premium === false ? (
-            <span className="btn btn-ghost btn-sm max-sm:text-0.5xl text-xl">Premium</span>
+          {!premium === true ? (
+            <span className="btn btn-ghost btn-sm max-sm:text-0.5xl text-xl px-2">Premium</span>
           ) : (
             <></>
           )}
